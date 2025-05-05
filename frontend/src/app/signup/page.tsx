7750fa739745c156d48ae8ddc0b704dc70ac8be4
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2025-05-05 18:33:36 +0300

    Create login and signup form

---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,40 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen w-full justify-center">
+      <div className="flex min-h-screen w-full max-w-7xl items-center justify-center p-6 md:p-10">
+        <form className="w-full max-w-sm space-y-4 rounded-lg border border-muted-foreground/50 p-6 shadow-lg sm:p-8 lg:p-10">
+          <div className="flex flex-col gap-6">
+            <div className="space-y-1">
+              <h1 className="text-xl font-bold">Sign Up</h1>
+              <p className="text-xs font-medium text-wrap text-muted-foreground">
+                Enter your details below to create your account
+              </p>
+            </div>
+            <div className="flex flex-col gap-3">
+              <div>
+                <label className="font-medium">Email</label>
+                <Input
+                  type="email"
+                  variant="custom"
+                  className="mt-1"
+                  placeholder="example@gmail.com"
+                />
+              </div>
+
+              <div>
+                <label className="font-medium">Password</label>
+                <Input variant="custom" type="password" className="mt-1" />
+              </div>
+            </div>
+            <Button className="w-full py-5 font-semibold">Sign Up</Button>
+          </div>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default page;
