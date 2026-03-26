4a98d91bad503259d7fb6743b33bf1b561ad490d
Pereslavtsev Vladimir <vovapereslavtsev@gmail.com>
2026-03-26 12:38:26 +0300

    refactoring: Change type for layout props

--- src/app/(secured)/(sidebar)/notes/[noteId]/layout.tsx
+++ src/app/(secured)/(sidebar)/notes/[noteId]/layout.tsx
@@ -1,19 +1,21 @@
 import CoverImageModal from '@/components/modals/cover-image-modal/cover-image-modal';
 import NotePageHeader from '@/components/pages/note-page/header/note-page-header';
+import PreloadedNoteProvider from '@/components/providers/preloaded-note-provider';
 import { preloadAuthQuery } from '@/lib/auth-server';
 import { api } from '../../../../../../convex/_generated/api';
 import { Id } from '../../../../../../convex/_generated/dataModel';
-import PreloadedNoteProvider from '@/components/providers/preloaded-note-provider';
 
 export default async function NotePageLayout({
   children,
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ noteId: Id<'notes'> }>;
+  params: Promise<{ noteId: string }>;
 }>) {
   const { noteId } = await params;
-  const preloadedQuery = preloadAuthQuery(api.notes.findNote, { id: noteId });
+  const preloadedQuery = preloadAuthQuery(api.notes.findNote, {
+    id: noteId as Id<'notes'>,
+  });
   return (
     <PreloadedNoteProvider preloadedQuery={preloadedQuery}>
       <NotePageHeader />
